f7d4672ac18ff110582b2735e951529af161f8e3
TascaBRX <lucastascasm23@gmail.com>
2025-08-13 10:36:19 -0300

    aprendendo sobre variaveis

---
+++ EstudandoVariaveis/variaveis.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+  int idade = 25;
+  float altura = 1.75;
+  char genero = 'M';
+
+  printf ("Idade: %d anos\n", idade);
+  printf ("Altura: %.2f metros\n", altura);
+  printf ("Gênero: %c\n", genero);
+
+  return 0;
+  
+
+}
