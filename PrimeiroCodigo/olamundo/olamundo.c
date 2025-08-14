2feb126022c5e5401c7ac33d1b0fcf92986dd29c
TascaBRX <lucastascasm23@gmail.com>
2025-08-14 13:35:30 -0300

    Adcionando comentarios explicando as linhas do codigo

--- PrimeiroCodigo/olamundo/olamundo.c
+++ PrimeiroCodigo/olamundo/olamundo.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+#include <stdio.h> // Comando para incluir a biblioteca padrão de entrada e saída e permitir o uso de printf, dentre outros. 
 
-int main (){
-  printf ("Olá, mundo!\n");
-  return 0; 
+// Este código é um exemplo simples de um programa em C que imprime "Olá, mundo!" na tela.
+int main (){ // Função principal do programa, onde a execução começa
+  printf ("Olá, mundo!\n"); // Função printf para imprimir a mensagem "Olá, mundo!" seguida de uma nova linha // \n é usado para criar uma nova linha
+  return 0; // Retorna 0 para indicar que o programa terminou com sucesso
+  // O programa termina aqui
 }
